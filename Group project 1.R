cc0431249f68113c3c0f5403c7b7316484beceda
FabioIaco <fabio.iacovone@student.uclouvain.be>
2025-01-04 20:09:35 +0100

    Update Group project 1.R

--- Group project 1.R
+++ Group project 1.R
@@ -1,3 +1,4 @@
+#1.1
 S0 <- 150        
 r <- 0.04       
 sigma <- 0.3    
@@ -24,3 +25,19 @@ payoffs[j] <- max(A_T - K, 0)
 
 V_t <- mean(payoffs) * exp(-r * T)
 
+#1.2
+s <- seq(0, T, length.out = m)
+d1_values <- numeric(m)
+d2_values <- numeric(m)
+
+for (i in 1:m) {
+  d1_values[i] <- (log(S_Ti[i] / K) + (r + (sigma^2) / 2) * (T - s[i])) / (sigma * sqrt(T - s[i]))
+  d2_values[i] <- d1_values[i] - sigma * sqrt(T - s[i])
+}
+
+v_call_values <- numeric(m)
+
+for (i in 1:m) {
+  v_call_values[i] <- max(S_Ti[i] * pnorm(d1_values[i]) - K * exp(-r * (T - s[i])) * pnorm(d2_values[i]), 0)
+}
+v_call_values
