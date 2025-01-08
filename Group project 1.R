fbcd39a2448e54e3fd8830e4174e7172db226fb6
FabioIaco <fabio.iacovone@student.uclouvain.be>
2025-01-08 18:24:25 +0100

    Update Group project 1.R

--- Group project 1.R
+++ Group project 1.R
@@ -76,3 +76,31 @@ V_t_up_in <- mean(payoffs_up_in) * exp(-r * T)
 V_t_up_out <- mean(payoffs_up_out) * exp(-r * T)
 print(V_t_up_in)
 print(V_t_up_out)
+#2.1
+delta <- T / m
+i_t <- floor(t / delta)
+alpha_t <- (i_t * delta - t) / delta
+
+sigma_A <- sqrt(
+  (sigma^2 * delta / m^2) * (
+    (m - i_t + 1)^2 * alpha_t +
+    (m - i_t) * (m - i_t + 1) * (2 * (m - i_t) + 1) / (6 * (m - i_t + 1))
+  )
+)
+
+sigma_A_squared <- (sigma^2 * delta / m^2) * (
+  (m - i_t + 1)^2 * alpha_t +
+  (m - i_t) * (m - i_t + 1) * (2 * (m - i_t) + 1) / (6 * (m - i_t + 1))
+)
+
+mu_A_t <- ((r - 0.5 * sigma^2) / m) * delta * (
+  (m - i_t + 1) * alpha_t + ((m - i_t) * (m - i_t + 1)) / 2
+) - (r - 0.5 * sigma_A_squared) * (T - t)
+
+S_t <- S0 * exp((r - 0.5 * sigma^2) * t)
+Y_t <- S_t * prod(S_Ti)
+X_t <- Y_t * exp(mu_A_t)
+
+d1 <- (log(S0 / K) + (r + 0.5 * sigma_A^2) * T) / (sigma_A * sqrt(T))
+d2 <- d1 - sigma_A * sqrt(T)
+V_t1 <- S_t * pnorm(d1) - K * exp(-r * T) * pnorm(d2)
